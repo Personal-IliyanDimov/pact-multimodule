422ea03f2dfc24ab8ff2bf38897c1d227b02db59
Iliyan Dimov <iliyan.dimov@zuhlke.com>
2024-02-21 16:36:37 +0200

    consumer and provider (part 3)

---
+++ rest-consumer/src/test/java/org/imd/consumer/ProfileClientTests.java
@@ -0,0 +1,90 @@
+package org.imd.consumer;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.PactSpecVersion;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.V4Pact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@SpringBootTest
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "ProfileProvider", pactVersion = PactSpecVersion.V4, port = "9999")
+public class ProfileClientTests {
+    @Autowired
+    private ProfileClient profileClient;
+
+    @Pact(consumer = "ProfileConsumer")
+    public V4Pact getAllProfiles(PactDslWithProvider builder) {
+        new PactDslJsonArray();
+        return builder
+            .given("profiles exists")
+                .uponReceiving("get all profiles")
+                .path("/profiles")
+            .willRespondWith()
+                .status(200)
+                .body(
+                    PactDslJsonArray.arrayEachLike()
+                        .integerType("id", 1)
+                        .stringType("name", "fake")
+                        .integerType("age", 10)
+                        .stringType("email", "fake@gmail.com")
+                        .date("dob")
+                    .closeObject()
+                )
+            .toPact(V4Pact.class);
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "getAllProfiles")
+    void testGetAllProfiles(MockServer mockServer) {
+        this.profileClient.setBaseUrl(mockServer.getUrl());
+        List<Profile> profiles = this.profileClient.getAllProfiles();
+        Assertions.assertThat(profiles.size()).isEqualTo(1);
+    }
+
+    @Pact(consumer = "ProfileConsumer")
+    public V4Pact getSingleProfile(PactDslWithProvider builder) {
+        return builder
+            .given("profiles 1 exists", "id", 1)
+                .uponReceiving("get profile with id 1")
+                .path("/profiles/1")
+            .willRespondWith()
+                .status(200)
+                .body(
+                    new PactDslJsonBody()
+                        .integerType("id", 1)
+                        .stringType("name", "fake")
+                        .integerType("age", 10)
+                        .stringType("email", "fake@gmail.com")
+                        .date("dob")
+                )
+            .toPact(V4Pact.class);
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "getSingleProfile")
+    void testGetSingleProfile(MockServer mockServer) {
+        this.profileClient.setBaseUrl(mockServer.getUrl());
+
+        Profile profile = this.profileClient.getSingleProfile(1);
+
+        Assertions.assertThat(profile.getId()).isEqualTo(1);
+        Assertions.assertThat(profile.getName()).isEqualToIgnoringCase("fake");
+        Assertions.assertThat(profile.getAge()).isEqualTo(10);
+        Assertions.assertThat(profile.getEmail()).isEqualToIgnoringCase("fake@gmail.com");
+        Assertions.assertThat(profile.getDob()).isInstanceOf(LocalDate.class);
+    }
+}
